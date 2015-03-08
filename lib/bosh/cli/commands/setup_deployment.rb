a8f6c9897e1b393910fd214bc6b3dff15a976c68
Dr Nic Williams <drnicwilliams@gmail.com>
2015-03-07 19:32:54 -0700

    when looking up deployments, filter by those including 'cf' release

--- lib/bosh/cli/commands/setup_deployment.rb
+++ lib/bosh/cli/commands/setup_deployment.rb
@@ -8,7 +8,9 @@ module Bosh::Cli::Command
       #   - get CC URL
       #   - get NATS servers + credentials
       #   - get DEA/runner security group (as the default SG)
-      cf_deployment_name ||= prompt_for_deployment
+      cf_deployment_name ||= prompt_for_deployment("cf")
+
+      say "Looking up '#{cf_deployment_name}'..."
       unless cf_manifest = director.get_deployment(cf_deployment_name)["manifest"]
         err "Deployment '#{cf_deployment_name}' has not completed successfully yet."
       end
@@ -16,15 +18,51 @@ module Bosh::Cli::Command
       unless cc_api_uri = cf["properties"]["cc"] && cf["properties"]["cc"]["srv_api_uri"]
         err "Deployment '#{cf_deployment_name}' is not Cloud Foundry. Missing properties.cc.srv_api_uri property."
       end
-      puts "CF API: #{cc_api_uri}"
+      unless system_domain = cf["properties"]["system_domain"]
+        err "Deployment '#{cf_deployment_name}' is not Cloud Foundry. Missing properties.system_domain property."
+      end
+      unless nats = cf["properties"]["nats"]
+        err "Deployment '#{cf_deployment_name}' is not Cloud Foundry. Missing properties.nats property."
+      end
+      # nats:
+      #   machines:
+      #   - 10.244.0.6
+      #   password: nats
+      #   port: 4222
+      #   user: nats
+
+      say "CF API: #{cc_api_uri}"
+      say "System domain: #{system_domain}"
+      say "NATS servers: #{nats["machines"].join(', ')}"
+
+      broker_api_hostname = "http://cf-containers-broker.#{system_domain}"
+      say "Broker API: #{broker_api_hostname}"
+      # TODO - generate this hostname, to allow docker-service to be deployed multiple times
+
+      # Next: select a CPI & subnet (via cyoi)
+      # Next: if AWS/OpenStack, get security group for the runner/dea_next jobs
+      # Next: choose an instance type (more epheral disk the better)
+      # Next: choose persistent disk size (to be shared amongst all services)
+      # Next: select which services to include (fewer = less docker images to fetch)
     end
 
 
     private
-      def prompt_for_deployment
+      def prompt_for_deployment(includes_release)
         names = director.list_deployments.map { |deployment| deployment["name"] }
+        # filter by includes_release
+        names = names.inject([]) do |list, deployment_name|
+          if manifest_yaml = director.get_deployment(deployment_name)["manifest"]
+            manifest = YAML.load(manifest_yaml)
+            releases = manifest["releases"].map {|rel| rel["name"]}
+            if releases.include?(includes_release)
+              list << deployment_name
+            end
+          end
+          list
+        end
         if names.size == 0
-          err "No deployments found. Please deploy Cloud Foundry first."
+          err "No Cloud Foundry deployments found. Please deploy Cloud Foundry first."
         elsif names.size == 1
           names.first
         else
